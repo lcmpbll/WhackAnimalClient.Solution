bc8f0510929743b04a5513de4a30b03551802d9a
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-18 13:09:47 -0700

    Add question to index, remove commented out code
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>
    co-authored-by: Caroline Cerussi <caroceru@gmail.com>

--- WhackAnimals/Models/ApiHelper.cs
+++ WhackAnimals/Models/ApiHelper.cs
@@ -2,26 +2,7 @@ using System.Threading.Tasks;
 using RestSharp;
 
 namespace WhackAnimals.Models
-{
-  // class ApiHelper
-  // {
-  //   public static async Task<string> GetAll()
-  //   {
-  //     RestClient client = new RestClient("http://localhhost:5004/api/v1/Facts"); //http://localhhost:5004/api/v1 ?
-  //     RestRequest request = new RestRequest($"facts", Method.GET);
-  //     var response = await client.ExecuteTaskAsync(request);
-  //     return response.Content;
-  //   }
-    
-  //    public static async Task<string> Get(int id)
-  //   {
-  //     RestClient client = new RestClient("http://localhost:5004/api");
-  //     RestRequest request =  new RestRequest($"facts/{id}", Method.GET);
-  //     var response =  await client.ExecuteTaskAsync(request);
-  //     return response.Content;
-  //   }
-  // }
-  
+{  
    class ApiHelper
   {
     public static async Task<string> GetAll()
@@ -34,7 +15,7 @@ namespace WhackAnimals.Models
 
     public static async Task<string> Get(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5004/api");
       RestRequest request = new RestRequest($"animals/{id}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
